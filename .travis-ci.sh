b2410b91298fd155235f712e4c51c03b8e0c9f28
John Toman <jtoman@cs.washington.edu>
2019-06-06 12:53:11 +0900

    Wrong version of z3...

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -8,7 +8,7 @@ opam install --yes menhir ppx_sexp_conv sexplib
 
 mkdir -p ~/.local/bin
 export PATH=$HOME/.local/bin:$PATH
-curl -L https://github.com/Z3Prover/z3/releases/download/z3-4.8.4/z3-4.8.4.d6df51951f4c-x64-debian-8.11.zip > z3.zip
-unzip -j z3.zip z3-4.8.4.d6df51951f4c-x64-debian-8.11/bin/z3 -d ~/.local/bin
+curl -L https://github.com/Z3Prover/z3/releases/download/z3-4.8.4/z3-4.8.4.d6df51951f4c-x64-ubuntu-14.04.zip > z3.zip
+unzip -j z3.zip z3-4.8.4.d6df51951f4c-x64-ubuntu-14.04/bin/z3 -d ~/.local/bin
 
 bash ./src/test_suite.sh
