74d55e5d11dd39576b01db5f7bc0302974cf0a5e
John Toman <jtoman@cs.washington.edu>
2019-07-17 15:26:30 +0900

    Factor out compilation
    Allows separate compilation for parsing/typechecking
    so we can get faster partial results when extending
    the language

--- src/option.ml
+++ src/option.ml
@@ -2,6 +2,10 @@ let map f = function
   | Some x -> Some (f x)
   | None -> None
 
+let iter f = function
+  | Some x -> f x
+  | None -> ()
+
 let value o ~default = match o with
   | Some o -> o
   | None -> default
