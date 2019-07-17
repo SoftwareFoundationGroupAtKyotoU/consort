ba87a30ea7025bfa16eb0c94f4f9ea25605b071a
John Toman <jtoman@cs.washington.edu>
2019-07-17 13:12:11 +0900

    Adds refinement that refers to function entry values
    This is accomplished with a kind of gross hack. We introduce
    a dummy variable for every leaf in every input leaf type, and
    then equate the leaf types with these dummy variables. When constraining
    the output type, we make the corresponding dummy variable one of the
    arguments to the output refinement.
    
    At calls, we introduce another dummy variable for each input leaf type,
    equate it with the path for the leaf type (if applicable) and add
    the dummy binding to the environment. This dummy variable is then
    used as "pre-value" argument to the output parameter.

--- src/paths.ml
+++ src/paths.ml
@@ -1,8 +1,9 @@
 open Sexplib.Std
 
 type 'a _const_ap = [
-  `AVar of string
-| `AProj of 'a * int
+  | `AVar of string
+  | `AProj of 'a * int
+  | `APre of string
 ] [@@deriving sexp]
 
 type const_ap = const_ap _const_ap [@@deriving sexp]
@@ -16,20 +17,30 @@ let rec to_z3_ident = function
   | `ADeref d -> Printf.sprintf "%s->*" @@ to_z3_ident d
   | `AProj (a,i) -> Printf.sprintf "%s->%d" (to_z3_ident a) i
   | `AVar v -> v
+  | `APre v -> Printf.sprintf "%s!old" v
+
+let rec pre = function
+  | `ADeref f -> `ADeref (pre f)
+  | `AProj (d,i) -> `AProj (pre d,i)
+  | `AVar v -> `APre v
+  | `APre v -> `APre v
 
 let t_ind a i = `AProj (a,i)
 
 let rec is_const_ap = function
+  | `APre _
   | `AVar _ -> true
   | `AProj (ap,_) -> is_const_ap ap
   | `ADeref _ -> false
 
 let rec has_root v = function
+  | `APre _ -> false
   | `AVar v' -> v = v'
   | `ADeref ap
   | `AProj (ap,_) -> has_root v ap
 
 let rec has_root_p p = function
   | `AVar v -> p v
+  | `APre _ -> false
   | `ADeref ap
   | `AProj (ap,_) -> has_root_p p ap
