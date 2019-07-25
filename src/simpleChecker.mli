1de115d28ba614cb89ff81bcb41a36af0dcc8ec5
John Toman <jtoman@cs.washington.edu>
2019-07-25 19:29:46 +0900

    Use explicit fold/unfold
    Removes the rectypes idea. What a mess.
    This is MUCH easier. No unfolding everywhere = smaller
    predicates

--- src/simpleChecker.mli
+++ src/simpleChecker.mli
@@ -1,1 +1,11 @@
-val typecheck_prog: SimpleTypes.funtyp StringMap.t -> Ast.prog -> SimpleTypes.funtyp StringMap.t* (int -> (SimpleTypes.r_typ StringMap.t) option) * int list
+
+module SideAnalysis: sig
+  type results = {
+    unfold_locs: Std.IntSet.t;
+    fold_locs: Std.IntSet.t
+  }
+end
+
+type simple_results = SimpleTypes.funtyp StringMap.t* (int -> (SimpleTypes.r_typ StringMap.t) option) * SideAnalysis.results
+
+val typecheck_prog: SimpleTypes.funtyp StringMap.t -> Ast.prog -> simple_results
