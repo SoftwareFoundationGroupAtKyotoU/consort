20d8c6c111e5612c147fad1b611d3dcb655c25ce
John Toman <jtoman@cs.washington.edu>
2019-07-30 11:02:39 +0900

    Adds flags for selecting the solver
    Also avoids generating the string NOT anywhere, because
    the hoice parser is LOL

--- src/verifyUtil.mli
+++ src/verifyUtil.mli
@@ -1,16 +1,23 @@
 module Options :
   sig
+    type solver =
+      | Hoice
+      | Spacer
+      | Z3SMT
+
     type t = {
-      debug_pred : bool;
-      debug_cons : bool;
-      debug_ast : bool;
-      save_cons : string option;
-      annot_infr : bool;
-      print_model : bool;
+      debug_pred: bool;
+      debug_cons: bool;
+      debug_ast: bool;
+      save_cons: string option;
+      annot_infr: bool;
+      print_model: bool;
       seq_solver: bool;
       check_trivial: bool;
-      dry_run: bool
+      dry_run : bool;
+      solver: solver
     }
+
     type arg_spec = (string * Arg.spec * string) list * (?comb:t -> unit -> t)
     val debug_arg_gen : unit -> arg_spec
     val solver_arg_gen : unit -> arg_spec
