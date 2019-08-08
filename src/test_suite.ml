3f567124d09074394cbc42d7d1c3f2ccf87da3dc
John Toman <jtoman@cs.washington.edu>
2019-08-08 17:00:05 +0900

    Makes output of consort friendlier
    Be clearer about why something failed

--- src/test_suite.ml
+++ src/test_suite.ml
@@ -1,10 +1,15 @@
+exception TestTimeout
+
 let run_test v_opts intr expect file =
-  if expect <> (Consort.check_file ~opts:v_opts ~intrinsic_defn:intr file) then
-    failwith @@
-      if expect then file ^ " did not verify as expected"
-      else file ^ " incorrectly verified"
-  else
-    ()
+  let res = Consort.check_file ~opts:v_opts ~intrinsic_defn:intr file in
+  let open Consort in
+  match res,expect with
+  | Verified,true -> ()
+  | Unverified (SolverError msg),_ -> failwith @@ "Solver failed with message: " ^ msg
+  | Unverified Timeout,_ -> raise TestTimeout
+  | Unverified _,false -> ()
+  | _,true -> failwith @@ Printf.sprintf "%s did not verify as expected" file
+  | _,false -> failwith @@ Printf.sprintf "%s incorrectly verified" file 
 
 let () =
   let expect = ref true in
@@ -32,6 +37,7 @@ let () =
           incr n_tests
         with
         | Failure s -> failwith @@ Printf.sprintf "Test %s failed with message: %s" f_name s
+        | TestTimeout -> Printf.printf "!!! WARNING: Test %s timed out, optimistically continuing... !!!\n" f_name; flush stdout
         | e -> failwith @@ Printf.sprintf "Test %s failed with unknown exception: %s " f_name @@ Printexc.to_string e
       else ()
     ) test_files;
