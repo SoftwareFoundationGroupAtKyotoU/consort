b4dfb7a78587dd7594c10ce967870c16b6327833
John Toman <jtoman@cs.washington.edu>
2019-09-03 13:05:28 +0900

    Filter out spacer overflow errors

--- src/test_suite.ml
+++ src/test_suite.ml
@@ -1,10 +1,15 @@
 exception TestTimeout
+exception InternalSpacerError
+
+let spacer_error_p msg =
+  msg = "(error \"tactic failed: Overflow encountered when expanding vector\")"
 
 let run_test v_opts intr expect file =
   let res = Consort.check_file ~opts:v_opts ~intrinsic_defn:intr file in
   let open Consort in
   match res,expect with
   | Verified,true -> ()
+  | Unverified (SolverError msg),_ when spacer_error_p msg -> raise InternalSpacerError
   | Unverified (SolverError msg),_ -> failwith @@ "Solver failed with message: " ^ msg
   | Unverified Timeout,_ -> raise TestTimeout
   | Unverified _,false -> ()
@@ -43,6 +48,7 @@ let () =
         with
         | Failure s -> failwith @@ Printf.sprintf "Test %s failed with message: %s" f_name s
         | TestTimeout -> Printf.printf "!!! WARNING: Test %s timed out, optimistically continuing... !!!\n" f_name; flush stdout
+        | InternalSpacerError -> Printf.printf "!!! WARNING: Test %s triggered a spacer bug, optimistically continuing... !!!\n" f_name; flush stdout
         | e ->
           Printexc.print_backtrace stdout;
           failwith @@ Printf.sprintf "Test %s failed with unknown exception: %s " f_name @@ Printexc.to_string e
