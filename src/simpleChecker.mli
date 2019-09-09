2a09ecbc8073e6c6f23ecd01bc470a83df16957e
John Toman <jtoman@cs.washington.edu>
2019-09-09 16:24:49 +0900

    Overhauls simple type checker
    Record let types, don't save environments (we don't need them)

--- src/simpleChecker.mli
+++ src/simpleChecker.mli
@@ -2,10 +2,11 @@
 module SideAnalysis: sig
   type results = {
     unfold_locs: Std.IntSet.t;
-    fold_locs: Std.IntSet.t
+    fold_locs: Std.IntSet.t;
+    let_types: SimpleTypes.r_typ Std.IntMap.t
   }
 end
 
-type simple_results = SimpleTypes.funtyp StringMap.t* (int -> (SimpleTypes.r_typ StringMap.t) option) * SideAnalysis.results
+type simple_results = SimpleTypes.funtyp StringMap.t * SideAnalysis.results
 
 val typecheck_prog: SimpleTypes.funtyp StringMap.t -> Ast.prog -> simple_results
