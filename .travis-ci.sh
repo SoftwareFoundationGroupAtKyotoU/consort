453fd8421384767b6755994d5fa480b1c532a49a
John Toman <jtoman@cs.washington.edu>
2019-11-22 10:10:12 +0900

    Adds unified tests to travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,7 +4,7 @@ set -ev
 
 opam init --yes --no-setup --comp 4.08.0
 eval $(opam config env)
-opam install --yes menhir ppx_sexp_conv sexplib dune ppx_let
+opam install --yes menhir ppx_sexp_conv sexplib dune ppx_let ppx_custom_printf
 
 mkdir -p ~/.local/bin
 export PATH=$HOME/.local/bin:$PATH
@@ -13,3 +13,4 @@ unzip -j z3.zip z3-4.8.4.d6df51951f4c-x64-ubuntu-14.04/bin/z3 -d ~/.local/bin
 
 # approximately the memory available on travis
 bash ./src/test_suite.sh -solver-args "-memory:6000"
+bash ./src/test_suite.sh -solver-args "-memory:6000" -mode unified
