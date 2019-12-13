965e2248aaf552bdb878940dc8b9f366dbc69771
John Toman <jtoman@cs.washington.edu>
2019-12-13 14:00:28 +0900

    Adds gradle wrapper

--- src/regnant/.travis-ci.sh
+++ src/regnant/.travis-ci.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-wget http://services.gradle.org/distributions/gradle-4.0.1-bin.zip
-unzip gradle-4.0.1-bin.zip
-export GRADLE_HOME=$PWD/gradle-4.0.1
-export PATH=$GRADLE_HOME/bin:$PATH
-
-gradle allDeps
+$(cd $(dirname $0) && pwd)/gradlew allDeps
