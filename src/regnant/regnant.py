6e8057b630a6b0cf42ea2d02c8225d52542ec744
John Toman <jtoman@cs.washington.edu>
2019-12-16 09:25:57 +0900

    Log commands run

--- src/regnant/regnant.py
+++ src/regnant/regnant.py
@@ -6,9 +6,15 @@ import tempfile
 import atexit, shutil, os
 import argparse
 
+def log_command(args, cmd):
+    if args.verbose:
+        import pipes
+        print "executing:"," ".join([ pipes.quote(s) for s in cmd ])
+
 def main(this_dir, args):
     parser = argparse.ArgumentParser()
     parser.add_argument("--work-dir")
+    parser.add_argument("--verbose", action="store_true")
     parser.add_argument("src_dir")
     parser.add_argument("entry_point")
     parser.add_argument("consort_args", nargs="*")
@@ -27,13 +33,16 @@ def main(this_dir, args):
         work_dir = args.work_dir
 
     cls_dir = os.path.join(work_dir, "classes")
-    os.makedirs(cls_dir)
+    if not os.path.exists(cls_dir):
+        os.makedirs(cls_dir)
 
     source_file = cls.replace(".", "/") + ".java"
 
+    compile_command = ["javac", "-g:lines,vars", "-d", cls_dir, args.src_dir + "/" + source_file]
+    log_command(args, compile_command)
     print "compiling source java...",
     sys.stdout.flush()
-    subprocess.check_call(["javac", "-d", cls_dir, args.src_dir + "/" + source_file])
+    subprocess.check_call(compile_command)
     print "done"
 
     flags = os.path.join(work_dir, "control.sexp")
@@ -48,15 +57,15 @@ def main(this_dir, args):
         "-f", "n", # no output
         "-no-bodies-for-excluded", # don't load the JCL (for now)
         "-w", # whole program mode
-        "-p", "cg", "off", # don't build a call graph
+        "-p", "cg.spark", "on", # run points to analysis
+        "-p", "jb", "use-original-names:true", # try to make our names easier
         "-pp", # use our classpath for system classes
         "-soot-class-path", cls_dir, # where to find the test file
         "-p", "wjtp.regnant", regnant_options,
         cls # the class to run on
     ]
 
-    import pipes
-    #print "executing:"," ".join([ pipes.quote(s) for s in regnant_command ])
+    log_command(args, regnant_command)
     print "Translating java bytecode...",
     sys.stdout.flush()
     with open("/dev/null", "w") as o:
@@ -73,6 +82,7 @@ def main(this_dir, args):
         intr_loc,
         "generated.smt"
     ]
+    log_command(args, intr_command)
     subprocess.check_call(intr_command)
     print "done"
     print "Running ConSORT on translated program:"
@@ -83,7 +93,7 @@ def main(this_dir, args):
     ] + args.consort_args + [
         data
     ]
-
+    log_command(args, consort_cmd)
     subprocess.call(consort_cmd)
 
 if __name__ == "__main__":
