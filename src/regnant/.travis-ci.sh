13a41b5e2e2539435539c45e9d267cf86310f61a
John Toman <jtoman@cs.washington.edu>
2019-12-19 10:45:54 +0900

    Friendlier console output for travis

--- src/regnant/.travis-ci.sh
+++ src/regnant/.travis-ci.sh
@@ -3,7 +3,7 @@
 set -ev
 
 THIS_DIR=$(cd $(dirname $0) && pwd);
-$THIS_DIR/gradlew -p $THIS_DIR installDist
+$THIS_DIR/gradlew --console plain -p $THIS_DIR installDist
 
 if [[ ! -d $HOME/jdk8/jdk8u232-b09 ]]; then
         curl -L "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u232b09.tar.gz" > /tmp/jdk.tar.gz;
