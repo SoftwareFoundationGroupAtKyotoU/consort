ad5d7d68f0b3eb2f748ee9e343a555d3d7da328d
John Toman <jtoman@cs.washington.edu>
2020-01-07 11:01:08 +0900

    Adds yaml output

--- src/test.ml
+++ src/test.ml
@@ -7,17 +7,45 @@ let () =
   in
   let (intr_fl,loader) = Intrinsics.option_loader () in
   let return_status = ref false in
+  let yaml_result = ref false in
   let spec = flags @ intr_fl @ [
       ("-cfa", Arg.Set_int KCFA.cfa, "k to use for k-cfa inference");
-      ("-exit-status", Arg.Set return_status, "Indicate successful verification with exit code")
+      ("-exit-status", Arg.Set return_status, "Indicate successful verification with exit code");
+      ("-yaml", Arg.Set yaml_result, "Print verification result in YAML format");
   ] in
   let target_name = ref None in
   Arg.parse spec (fun s -> target_name := Some s) "Verify imp file";
   match !target_name with
-  | None -> print_endline "No file provided"
+  | None -> print_endline "No file provided"; exit 1
   | Some in_name -> 
     let res = Consort.check_file ~opts:(to_opts()) ~intrinsic_defn:(loader ()) in_name in
-    print_endline @@ Consort.result_to_string res;
+    let () = 
+      if !yaml_result then
+        let yaml_repr =
+          let open Consort in
+          match res with
+          | Verified ->
+            `O [
+                ("result", `Bool true)
+              ]
+          | Unverified r ->
+            let expl = match r with
+              | Timeout -> [
+                ("reason", `String "timeout")
+              ]
+              | Unsafe -> [ "reason", `String "unsafe" ]
+              | UnhandledSolverOutput s -> [ ("reason", `String "unhandled"); ("msg", `String s) ]
+              | SolverError s -> [ ("reason", `String "solver-error"); ("msg", `String s) ]
+              | Aliasing -> [ ("reason", `String "ownership") ]
+              | Unknown -> [ "reason", `String "unknown" ]
+            in
+            `O ([ "result", `Bool false ] @ expl)
+        in
+        print_endline @@ Yaml.to_string_exn yaml_repr
+      else
+        print_endline @@ Consort.result_to_string res;
+    in
+    
     if !return_status then
       match res with
       | Consort.Verified -> exit 0
