89edf1ae0f9c0ab4eb66a43e721be21635397f0f
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-09-16 11:02:17 +0900

    Remove one of the pointless tests

--- src/regnant/src/integration/java/ObjectEquality2.java
+++ src/regnant/src/integration/java/ObjectEquality2.java
@@ -1,15 +1,15 @@
 import data.C;
 
-public class ObjectEquality2{
-    public static void main(String[] args){
+public class ObjectEquality2 {
+    public static void main(String[] args) {
         C a = new C();
         C b = a;
         a.setJ(12);
-        
-        if(a == b){
-            a.setJ(15);
-        }else{
-            b.setJ(6);
+
+        if (a == b) {
+            assert (a.j == b.j);
+        } else {
+            a.setJ(5);
         }
     }
 }
--- src/regnant/src/integration/java/ObjectEquality3.java
+++ src/regnant/src/integration/java/ObjectEquality3.java
@@ -1,15 +1,12 @@
 import data.C;
 
-public class ObjectEquality3{
-    public static void main(String[] args){
+public class ObjectEquality3 {
+    public static void main(String[] args) {
         C a = new C();
         C b = a;
         a.setJ(12);
-        
-        if(a == b){
-            assert(a.j == b.j);
-        }else{
-            a.setJ(5);
-        }
+
+        // won't work unless ifptr is implemented differently
+        // assert(a == b);
     }
 }
