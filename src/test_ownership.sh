4c26764f7c8e274e633ce6b5ff749e06333617d1
Hideto Ueno <uenoku.tokotoko@gmail.com>
2020-11-26 00:55:06 +0900

    add test ownership script

---
+++ src/test_ownership.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+THIS_DIR=$(cd $(dirname $0) && pwd)
+
+make -C $THIS_DIR stdlib/lin.intr ownership.exe > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "! WARNING: BUILD FAILED" >&2
+fi
+$THIS_DIR/_build/default/ownership.exe -intrinsics $THIS_DIR/stdlib/lin.intr "$@"
