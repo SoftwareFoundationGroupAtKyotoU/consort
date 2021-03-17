d4ad49ff02854f50dabd502fa78eb45f30d1287d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-03-17 12:45:58 +0900

    Fix ArgOptions.Solver.opt_gen

--- src/argOptions.ml
+++ src/argOptions.ml
@@ -17,14 +17,19 @@ module Solver = struct
     let timeout = ref default.timeout in
     let command = ref None in
     let extra = ref None in
-    ([
-      ("-" ^ pref ^ "timeout", Set_int timeout, Printf.sprintf "Timeout for %s in seconds" solv_nm);
-      ("-" ^ pref ^ "command", String (fun s -> command := Some s), Printf.sprintf "Executable for %s" solv_nm);
-      ("-" ^ pref ^ "solver-args", String (fun s -> extra := Some s), Printf.sprintf "Extra arguments to pass wholesale to %s" solv_nm)
-    ], fun ?comb:_ () ->
-        {
-          timeout = !timeout;
-          command = !command;
-          command_extra = !extra
-        })
+    let spec = [
+      ("-" ^ pref ^ "timeout", Set_int timeout,
+       Printf.sprintf "Timeout for %s in seconds" solv_nm);
+      ("-" ^ pref ^ "command", String (fun s -> command := Some s),
+       Printf.sprintf "Executable for %s" solv_nm);
+      ("-" ^ pref ^ "solver-args", String (fun s -> extra := Some s),
+       Printf.sprintf "Extra arguments to pass wholesale to %s" solv_nm)
+    ] in
+    let update ?(comb=default) () =
+      let _ = comb in {
+        timeout = !timeout;
+        command = !command;
+        command_extra = !extra
+      } in
+    (spec, update)
 end
