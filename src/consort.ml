5d43f6fd18c5b053d1db92337f2d4c1d47025962
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-03-30 19:13:45 +0900

    Add print_typecheck

--- src/consort.ml
+++ src/consort.ml
@@ -141,6 +141,32 @@ let print_ownership ownership_res infer_res ast =
     let o_printer = pf "%f" in
     print_program ~o_map ~o_printer infer_res ast
 
+let print_typecheck (f_types, side) ast =
+  let open Ast in
+  let open PrettyPrint in
+  let open SimpleChecker.SideAnalysis in
+  let open SimpleTypes in
+  let annot_fn f_name =
+    let s = fntype_to_string @@ StringMap.find f_name f_types in
+    pl [pf "/* %s */" s; newline] in
+  let from_ty_patt ty patt =
+    let rec patt_loop acc p t =
+      match p, t with
+      | PVar v, _ -> (pf "%s: %s" v @@ type_to_string t)::acc
+      | PNone, _ -> acc
+      | PTuple pl, `Tuple tl -> List.fold_left2 patt_loop acc pl tl
+      | _, _ -> assert false in
+    let ty_list = patt_loop [] patt ty |> List.rev in
+    match List.length ty_list with
+    | 0 -> pl [ps "/* (no bindings) */"; newline]
+    | 1 -> pl [ps "/* "; pl ty_list; ps " */"; newline]
+    | _ -> pcomment ~body:(psep_gen newline ty_list) in
+  let annot (id, _) e =
+    match Std.IntMap.find_opt id side.let_types, e with
+    | Some ty, Let (patt, _, _) -> from_ty_patt ty patt
+    | _ -> null in
+  AstPrinter.pretty_print_program ~annot_fn ~annot stdout ast
+
 let to_hint o_res record =
   let open OwnershipInference in
   let o_map = function
@@ -194,28 +220,5 @@ let typecheck ~opts file =
   let intr_op = (ArgOptions.get_intr opts).op_interp in
   let simple_op = RefinementTypes.to_simple_funenv intr_op in
   let simple_res = SimpleChecker.typecheck_prog simple_op ast in
-  let f_types, side = simple_res in
-  let open Ast in
-  let open PrettyPrint in
-  let open SimpleTypes in
-  let annot_fn f_name =
-    let s = fntype_to_string @@ StringMap.find f_name f_types in
-    pl [pf "/* %s */" s; newline] in
-  let from_ty_patt ty patt =
-    let rec patt_loop acc p t =
-      match p, t with
-      | PVar v, _ -> (pf "%s: %s" v @@ type_to_string t)::acc
-      | PNone, _ -> acc
-      | PTuple pl, `Tuple tl -> List.fold_left2 patt_loop acc pl tl
-      | _, _ -> assert false in
-    let ty_list = patt_loop [] patt ty |> List.rev in
-    match List.length ty_list with
-    | 0 -> pl [ps "/* (no bindings) */"; newline]
-    | 1 -> pl [ps "/* "; pl ty_list; ps " */"; newline]
-    | _ -> pcomment ~body:(psep_gen newline ty_list) in
-  let annot (id, _) e =
-    match Std.IntMap.find_opt id side.let_types, e with
-    | Some ty, Let (patt, _, _) -> from_ty_patt ty patt
-    | _ -> null in
-  AstPrinter.pretty_print_program ~with_labels:true ~annot_fn ~annot stdout ast;
+  print_typecheck simple_res ast;
   Verified
