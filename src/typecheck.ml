26e9e384e10461834aeea24a29ef4b6e722bc1a6
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-03-30 18:13:39 +0900

    Remove typecheck.exe

--- src/typecheck.ml
+++
@@ -1,6 +0,0 @@
-let () =
-  let n = ref None in
-  let opts = ArgOptions.parse (fun s -> n := Some s) "Parse and (simple) typecheck <file>" in
-  match !n with
-  | None -> print_endline "No file provided"
-  | Some f -> let _ = Consort.typecheck ~opts f in ()
