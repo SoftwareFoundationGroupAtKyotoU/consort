db10a55e1f3677a000b9cd94ce74fd072a3a371f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-03-30 19:00:02 +0900

    Add Consort.ownership

--- src/consort.mli
+++ src/consort.mli
@@ -14,4 +14,5 @@ val reason_to_string : reason -> bool -> string
 val result_to_string : check_result -> string
 
 val consort : opts:ArgOptions.t -> string -> check_result
+val ownership : opts:ArgOptions.t -> string -> check_result
 val typecheck : opts:ArgOptions.t -> string -> check_result
