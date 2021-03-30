2cf05f09f2ea00b921e4474d686fdb8944bfb71b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-03-30 18:04:51 +0900

    Update typecheck

--- src/typecheck.ml
+++ src/typecheck.ml
@@ -1,41 +1,36 @@
-open Ast
-
 let typecheck ~opts file =
-  let (fn,prog) = AstUtil.parse_file file in
-  let simple_op = RefinementTypes.to_simple_funenv (ArgOptions.get_intr opts).op_interp in
-  let f_types,SimpleChecker.SideAnalysis.{let_types; _ } = SimpleChecker.typecheck_prog simple_op (fn,prog) in
+  let ast = AstUtil.parse_file file in
+  let intr_op = (ArgOptions.get_intr opts).op_interp in
+  let simple_op = RefinementTypes.to_simple_funenv intr_op in
+  let simple_res = SimpleChecker.typecheck_prog simple_op ast in
+  let f_types, side = simple_res in
+  let open Ast in
   let open PrettyPrint in
-  AstPrinter.pretty_print_program ~with_labels:true ~annot_fn:(fun f_name ff ->
-    pl [
-      pf "/* %s */" @@ SimpleTypes.fntype_to_string @@ StringMap.find f_name f_types;
-      newline
-    ] ff
-  ) ~annot:(fun (id,_) e ->
-    let open Std in
-    match IntMap.find_opt id let_types,e with
-    | Some ty,Let (patt,_,_) ->
-      let rec patt_loop acc p t =
-        match p,t with
-        | PVar v,_ -> (pf "%s: %s" v @@ SimpleTypes.type_to_string t)::acc
-        | PNone,_ -> acc
-        | PTuple pl,`Tuple tl ->
-          List.fold_left2 patt_loop acc pl tl
-        | _,_ -> assert false
-      in
-      let ty_list = patt_loop [] patt ty |> List.rev in
-      let sz = List.length ty_list in
-      if sz = 0 then
-        pl [ ps "/* (no bindings) */"; newline ]
-      else if sz = 1 then
-        pl [ ps "/* "; pl ty_list; ps " */"; newline ]
-      else
-        pblock
-          ~nl:true
-          ~op:(ps "/*")
-          ~body:(psep_gen newline ty_list)
-          ~close:(ps "*/")
-    | _ -> null
-  ) stdout (fn,prog)
+  let annot_fn f_name =
+    let s = SimpleTypes.fntype_to_string @@ StringMap.find f_name f_types in
+    pl [pf "/* %s */" s; newline] in
+  let from_ty_patt ty patt =
+    let rec patt_loop acc p t =
+      match p, t with
+      | PVar v, _ -> (pf "%s: %s" v @@ SimpleTypes.type_to_string t)::acc
+      | PNone, _ -> acc
+      | PTuple pl, `Tuple tl -> List.fold_left2 patt_loop acc pl tl
+      | _, _ -> assert false in
+    let ty_list = patt_loop [] patt ty |> List.rev in
+    match List.length ty_list with
+    | 0 -> pl [ps "/* (no bindings) */"; newline]
+    | 1 -> pl [ps "/* "; pl ty_list; ps " */"; newline]
+    | _ -> pblock
+             ~nl:true
+             ~op:(ps "/*")
+             ~body:(psep_gen newline ty_list)
+             ~close:(ps "*/")
+  in
+  let annot (id, _) e =
+    match Std.IntMap.find_opt id side.let_types, e with
+    | Some ty, Let (patt, _, _) -> from_ty_patt ty patt
+    | _ -> null in
+  AstPrinter.pretty_print_program ~with_labels:true ~annot_fn ~annot stdout ast
 
 let () =
   let n = ref None in
