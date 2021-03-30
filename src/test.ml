c4ddedbeb58f944738297c265311832418f1f774
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-03-30 15:06:21 +0900

    Add result_to_{yaml,exit}

--- src/test.ml
+++ src/test.ml
@@ -1,3 +1,29 @@
+let result_to_yaml =
+  let open Consort in
+  function
+  | Verified -> `O [("result", `Bool true)]
+  | Unverified r ->
+    let expl = match r with
+      | Timeout ->
+        [("reason", `String "timeout")]
+      | Unsafe ->
+        [("reason", `String "unsafe")]
+      | UnhandledSolverOutput s ->
+        [("reason", `String "unhandled"); ("msg", `String s)]
+      | SolverError s ->
+        [("reason", `String "solver-error"); ("msg", `String s)]
+      | Aliasing ->
+        [("reason", `String "ownership")]
+      | Unknown ->
+        [("reason", `String "unknown")] in
+    `O ([("result", `Bool false)] @ expl)
+
+let result_to_exit =
+  let open Consort in
+  function
+  | Verified -> exit 0
+  | Unverified _ -> exit 1
+
 let () =
   let target_name = ref None in
   let opts = ArgOptions.parse (fun s -> target_name := Some s) "Verify imp file" in
@@ -7,33 +33,11 @@ let () =
     let res = Consort.check_file ~opts in_name in
     let () =
       if opts.yaml then
-        let yaml_repr =
-          let open Consort in
-          match res with
-          | Verified ->
-            `O [
-              ("result", `Bool true)
-            ]
-          | Unverified r ->
-            let expl = match r with
-              | Timeout -> [
-                  ("reason", `String "timeout")
-                ]
-              | Unsafe -> [ "reason", `String "unsafe" ]
-              | UnhandledSolverOutput s -> [ ("reason", `String "unhandled"); ("msg", `String s) ]
-              | SolverError s -> [ ("reason", `String "solver-error"); ("msg", `String s) ]
-              | Aliasing -> [ ("reason", `String "ownership") ]
-              | Unknown -> [ "reason", `String "unknown" ]
-            in
-            `O ([ "result", `Bool false ] @ expl)
-        in
-        print_endline @@ Yaml.to_string_exn yaml_repr
+        print_endline @@ Yaml.to_string_exn @@ result_to_yaml res
       else
         print_endline @@ Consort.result_to_string res;
     in
     if opts.exit_status then
-      match res with
-      | Consort.Verified -> exit 0
-      | Consort.Unverified _ -> exit 1
+      result_to_exit res
     else
       ()
