dbd52e8ac4a0246f22bba2400de9f215f7c082ee
igarashi <igarashi@kuis.kyoto-u.ac.jp>
2021-09-06 20:59:53 +0900

    Use z3 (4.8.11) from opam

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,12 +4,12 @@ set -ev
 
 opam init --yes --no-setup --comp 4.08.0
 eval $(opam config env)
-opam install --yes menhir ppx_sexp_conv sexplib dune ppx_let ppx_custom_printf yaml
+opam install --yes menhir ppx_sexp_conv sexplib dune ppx_let ppx_custom_printf yaml z3
 
 mkdir -p ~/.local/bin
 export PATH=$HOME/.local/bin:$PATH
-curl -L https://github.com/Z3Prover/z3/releases/download/z3-4.8.12/z3-4.8.12-x64-glibc-2.31.zip > z3.zip
-unzip -o -j z3.zip z3-4.8.12-x64-glibc-2.31/bin/z3 -d ~/.local/bin
+# curl -L https://github.com/Z3Prover/z3/releases/download/z3-4.8.12/z3-solver-4.8.12.0.tar.gz > z3.tar.gz
+# unzip -o -j z3.zip z3-4.8.12-x64-glibc-2.31/bin/z3 -d ~/.local/bin
 
 z3 -version
 
