602f61ce240545d7684e614838b5ddb8e2d6e80d
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2021-09-06 21:47:53 +0900

    Move opam install

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,13 +2,12 @@
 
 set -ev
 
-opam init --yes --no-setup --comp 4.08.0
-eval $(opam config env)
-opam install --yes menhir ppx_sexp_conv sexplib dune ppx_let ppx_custom_printf yaml
-travis_wait 30 install --yes z3
+# opam init --yes --no-setup --comp 4.08.0
+# eval $(opam config env)
+# opam install --yes menhir ppx_sexp_conv sexplib dune ppx_let ppx_custom_printf yaml z3
 
-mkdir -p ~/.local/bin
-export PATH=$HOME/.local/bin:$PATH
+# mkdir -p ~/.local/bin
+# export PATH=$HOME/.local/bin:$PATH
 # curl -L https://github.com/Z3Prover/z3/releases/download/z3-4.8.12/z3-solver-4.8.12.0.tar.gz > z3.tar.gz
 # unzip -o -j z3.zip z3-4.8.12-x64-glibc-2.31/bin/z3 -d ~/.local/bin
 
