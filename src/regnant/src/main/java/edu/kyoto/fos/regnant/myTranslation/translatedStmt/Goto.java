bf7c8b10d4d1f35eff6d51aa06605911743bb379
Ryota Kobayashi <ryota5884@icloud.com>
2022-01-20 19:19:11 +0900

    add comment

--- src/regnant/src/main/java/edu/kyoto/fos/regnant/myTranslation/translatedStmt/Goto.java
+++ src/regnant/src/main/java/edu/kyoto/fos/regnant/myTranslation/translatedStmt/Goto.java
@@ -7,7 +7,9 @@ import soot.jimple.internal.JGotoStmt;
 import java.util.HashMap;
 import java.util.List;
 
+// 変換後の Goto 式を表すクラス
 public class Goto implements TranslatedUnit {
+	// funcName は現在制御が渡っている元々の関数名, target は Goto で向かう基本ブロックを表す.
 	private final String funcName;
 	private final BasicBlock target;
 
